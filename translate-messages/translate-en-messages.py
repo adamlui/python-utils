adamlui/python-utils

commit 1836ea87d122adfed53064424d3e5cf5b0902639

Removed UI colorization for readability

translate-messages/translate-en-messages.py
@@ -1,6 +1,6 @@
 '''
 Script:       translate-en-messages.py
-Version:      2023.11.14
+Version:      2024.4.28
 Description:  Translate msg's from en/messages.json to [[output_langs]/messages.json]
 Author:       Adam Lui
 URL:          https://github.com/adamlui/python-utils
@@ -14,7 +14,6 @@ locales_folder = '_locales' ; provider = ''
 target_langs = ['af', 'am', 'ar', 'az', 'be', 'bem', 'bg', 'bn', 'bo', 'bs', 'ca', 'ceb', 'ckb', 'cs', 'cy', 'da', 'de', 'dv', 'dz', 'el', 'en', 'en-GB', 'eo', 'es', 'et', 'eu', 'fa', 'fi', 'fo', 'fr', 'gd', 'gl', 'gu', 'haw', 'he', 'hi', 'hr', 'ht', 'hu', 'hy', 'id', 'is', 'it', 'ja', 'ka', 'kab', 'kk', 'km', 'kn', 'ko', 'ku', 'ky', 'la', 'lb', 'lo', 'lt', 'lv', 'mg', 'mi', 'mk', 'ml', 'mn', 'ms', 'mt', 'my', 'ne', 'nl', 'no', 'ny', 'pa', 'pap', 'pl', 'ps', 'pt', 'ro', 'ru', 'rw', 'sg', 'si', 'sk', 'sl', 'sm', 'sn', 'so', 'sr', 'sv', 'sw', 'ta', 'te', 'tg', 'th', 'ti', 'tk', 'tn', 'to', 'tpi', 'tr', 'uk', 'ur', 'uz', 'vi', 'xh', 'yi', 'zh', 'zh-CN', 'zh-HK', 'zh-SG', 'zh-TW', 'zu']
 
 # UI initializations
-os.system('color') ; print('\033[0;92m') # set font to bright green
 terminal_width = os.get_terminal_size()[0]
 def print_trunc(msg) : print(msg if len(msg) < terminal_width else msg[0:terminal_width-4] + '...')
 

commit 4991f28d7517b9f77519e9d7440df53576cac990

Replaced `stdout` methods of overwriting w/ `print`
Fixes 1836ea87d122adfed53064424d3e5cf5b0902639 caused no more overwriting

translate-messages/translate-en-messages.py
@@ -1,13 +1,12 @@
 '''
 Script:       translate-en-messages.py
-Version:      2024.4.29
+Version:      2024.4.29.1
 Description:  Translate msg's from en/messages.json to [[output_langs]/messages.json]
 Author:       Adam Lui
 URL:          https://github.com/adamlui/python-utils
 '''
 
 import os, json
-from sys import stdout # for dynamic prints
 from translate import Translator
 
 locales_folder = '_locales' ; provider = ''
@@ -88,8 +87,7 @@ for lang_code in output_langs:
     else : messages = {}
 
     # Attempt translations
-    stdout.write(f"{ 'Adding' if not messages else 'Updating' } { folder }/messages.json...\r")
-    stdout.flush()
+    print(f"{ 'Adding' if not messages else 'Updating' } { folder }/messages.json...", end='\r')
     en_keys = list(en_messages.keys())
     fail_flags = ['INVALID TARGET LANGUAGE', 'TOO MANY REQUESTS', 'MYMEMORY']
     for key in en_keys:
@@ -124,9 +122,7 @@ for lang_code in output_langs:
     if translated_msgs == messages : langs_skipped.append(lang_code) ; lang_skipped = True
     elif translated_msgs != messages : langs_translated.append(lang_code) ; lang_translated = True
     if not lang_translated : langs_not_translated.append(lang_code)
-    stdout.write(' ' * terminal_width + '\r') # erase prev line
-    stdout.write(f"{ 'Added' if lang_added else 'Skipped' if lang_skipped else 'Updated' } { folder }/messages.json\n")
-    stdout.flush()
+    print(f"{ 'Added' if lang_added else 'Skipped' if lang_skipped else 'Updated' } { folder }/messages.json")
 
 # Print final summary
 print_trunc('\nAll messages.json files updated successfully!\n')