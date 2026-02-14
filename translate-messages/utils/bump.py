f83e3254e47e73533068985ed52c8ae6c99dcda1
Adam Lui <adam@kudoai.com>
2026-02-14 09:10:56 -0800

    Added `update_readme_versions`, improved log msgs

--- translate-messages/utils/bump.py
+++ translate-messages/utils/bump.py
@@ -1,5 +1,5 @@
 from os import path
-import sys
+import re, sys
 from types import SimpleNamespace as sns
 import tomli, tomli_w
 
@@ -17,10 +17,19 @@ def update_changelog_url():
     new_ver_tag = f'{project.name}-{project.version}'
     new_changelog_url = f"{project.urls['Releases']}/tag/{new_ver_tag}"
     log.data(f'Generated changelog URL: {new_changelog_url}')
-    log.info(f"{ 'Updating' if 'Changelog' in project.urls else 'Adding new' } Changelog URL...")
+    log.info(f"{ 'Updating' if 'Changelog' in project.urls else 'Adding new' } Changelog URL in pyproject.toml...")
     project.urls['Changelog'] = new_changelog_url
     pyproject['project'] = vars(project) # update og dict for dumping
     with open(pyproject_path, 'wb') as file : tomli_w.dump(pyproject, file)
-    log.success(f'Bumped changelog URL ver tag to [{new_ver_tag}]!')
+    log.success(f'Bumped Changelog URL version tag to [{new_ver_tag}]!')
+
+def update_readme_versions():
+    readme_path = path.join(path.dirname(__file__), '../README.md')
+    log.info('Updating versions in README.md...')
+    with open(readme_path, 'r', encoding='utf-8') as file : readme_content = file.read()
+    updated_readme_content = re.sub(r'\d+\.\d+\.\d+', project.version, readme_content)
+    with open(readme_path, 'w', encoding='utf-8') as file : file.write(updated_readme_content)
+    log.success(f'Updated versions in README URLs to [{project.version}]!')
 
 update_changelog_url()
+update_readme_versions()
