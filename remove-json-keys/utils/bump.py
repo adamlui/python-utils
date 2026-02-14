389bcf636486e1e710e434e5bf05071d0de4968c
Adam Lui <adam@kudoai.com>
2026-02-14 08:06:44 -0800

    Used sns

--- remove-json-keys/utils/bump.py
+++ remove-json-keys/utils/bump.py
@@ -1,29 +1,29 @@
 from os import path
+from types import SimpleNamespace as sns
 import sys
 import tomli, tomli_w
 
 sys.path.insert(0, path.join(path.dirname(__file__), '../src'))
-from remove_json_keys.lib import log # type: ignore
+from remove_json_keys.lib import log  # type: ignore
 
 pyproject_path = path.join(path.dirname(__file__), '../pyproject.toml')
-with open(pyproject_path, 'rb') as file:
-    pkg_name = tomli.load(file)['project']['name']
+log.info(f'Loading {pyproject_path}...')
+with open(pyproject_path, 'rb') as file : pyproject = tomli.load(file)
+project = sns(**pyproject['project'])
 
 def update_changelog_url():
-
-    log.info(f'Loading {pyproject_path}...')
-    with open(pyproject_path, 'rb') as file : pyproject = tomli.load(file)
-
-    ver_tag = f"{pkg_name}-{pyproject['project']['version']}"
+    ver_tag = f"{project.name}-{project.version}"
     changelog_url = f'https://github.com/adamlui/python-utils/releases/tag/{ver_tag}'
     log.data(f'Generated changelog URL: {changelog_url}')
 
-    if 'urls' not in pyproject['project']:
+    if not hasattr(project, 'urls'):
         log.info('Creating [project.urls] section...')
-        pyproject['project']['urls'] = {}
+        project.urls = {}
 
-    log.info(f"{ 'Updating' if 'Changelog' in pyproject['project']['urls'] else 'Adding new' } Changelog URL...")
-    pyproject['project']['urls']['Changelog'] = changelog_url
+    log.info(f"{ 'Updating' if 'Changelog' in project.urls else 'Adding new' } Changelog URL...")
+    project.urls['Changelog'] = changelog_url
+
+    pyproject['project'] = vars(project) # convert sns back to dict for dumping
     with open(pyproject_path, 'wb') as file : tomli_w.dump(pyproject, file)
 
     log.success(f"Bumped changelog URL ver tag to [{ver_tag}]!")
