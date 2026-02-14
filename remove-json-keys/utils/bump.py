5198d0c12a939b2631815874e7f40d0d2a3abab0
Adam Lui <adam@kudoai.com>
2026-02-14 08:55:06 -0800

    Alphabetized imports, prepended `new_` to template var names, used `project.urls['Releases']`

--- remove-json-keys/utils/bump.py
+++ remove-json-keys/utils/bump.py
@@ -1,26 +1,26 @@
 from os import path
-from types import SimpleNamespace as sns
 import sys
+from types import SimpleNamespace as sns
 import tomli, tomli_w
 
 # Init logger
 sys.path.insert(0, path.join(path.dirname(__file__), '../src'))
 from remove_json_keys.lib import log # type: ignore
 
-# Load pyproject.toml
+# Init project data
 pyproject_path = path.join(path.dirname(__file__), '../pyproject.toml')
 log.info(f'Loading {pyproject_path}...')
 with open(pyproject_path, 'rb') as file : pyproject = tomli.load(file)
 project = sns(**pyproject['project'])
 
 def update_changelog_url():
-    ver_tag = f'{project.name}-{project.version}'
-    changelog_url = f'https://github.com/adamlui/python-utils/releases/tag/{ver_tag}'
-    log.data(f'Generated changelog URL: {changelog_url}')
+    new_ver_tag = f'{project.name}-{project.version}'
+    new_changelog_url = f"{project.urls['Releases']}/tag/{new_ver_tag}"
+    log.data(f'Generated changelog URL: {new_changelog_url}')
     log.info(f"{ 'Updating' if 'Changelog' in project.urls else 'Adding new' } Changelog URL...")
-    project.urls['Changelog'] = changelog_url
+    project.urls['Changelog'] = new_changelog_url
     pyproject['project'] = vars(project) # update og dict for dumping
     with open(pyproject_path, 'wb') as file : tomli_w.dump(pyproject, file)
-    log.success(f'Bumped changelog URL ver tag to [{ver_tag}]!')
+    log.success(f'Bumped changelog URL ver tag to [{new_ver_tag}]!')
 
 update_changelog_url()
