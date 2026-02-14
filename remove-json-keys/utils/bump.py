f2840270cccd08fd19e214801f50e046e271aeac
Adam Lui <adam@kudoai.com>
2026-02-14 06:52:18 -0800

    Imported `logging` as shorter `log`

--- remove-json-keys/utils/bump.py
+++ remove-json-keys/utils/bump.py
@@ -1,7 +1,7 @@
-import os, logging
+import logging as log, os
 import tomli, tomli_w
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+log.basicConfig(level=log.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 pyproject_path = os.path.join(os.path.dirname(__file__), '../pyproject.toml')
 with open(pyproject_path, 'rb') as file:
@@ -9,21 +9,21 @@ with open(pyproject_path, 'rb') as file:
 
 def update_changelog_url():
 
-    logging.debug(f'Loading {pyproject_path}...')
+    log.debug(f'Loading {pyproject_path}...')
     with open(pyproject_path, 'rb') as file : pyproject = tomli.load(file)
 
     ver_tag = f"{pkg_name}-{pyproject['project']['version']}"
     changelog_url = f'https://github.com/adamlui/python-utils/releases/tag/{ver_tag}'
-    logging.debug(f'Generated changelog URL: {changelog_url}')
-    
+    log.debug(f'Generated changelog URL: {changelog_url}')
+
     if 'urls' not in pyproject['project']:
-        logging.debug('Creating [project.urls] section...')
+        log.debug('Creating [project.urls] section...')
         pyproject['project']['urls'] = {}
 
-    logging.debug(f"{ 'Updating' if 'Changelog' in pyproject['project']['urls'] else 'Adding new' } Changelog URL...")
+    log.debug(f"{ 'Updating' if 'Changelog' in pyproject['project']['urls'] else 'Adding new' } Changelog URL...")
     pyproject['project']['urls']['Changelog'] = changelog_url
     with open(pyproject_path, 'wb') as file : tomli_w.dump(pyproject, file)
 
-    logging.info(f"Bumped changelog URL ver tag to [{ver_tag}]!")
+    log.info(f"Bumped changelog URL ver tag to [{ver_tag}]!")
 
 update_changelog_url()
