8b93c74061f4891b71a5843126161d7b4acb55c0
Adam Lui <adam@kudoai.com>
2026-02-16 05:08:29 -0800

    Condensed `clean()`, moved to bottom

--- translate-messages/noxfile.py
+++ translate-messages/noxfile.py
@@ -7,23 +7,20 @@ def bump(session):
     session.run('python', 'utils/bump.py', *session.posargs)
 
 @nox.session(venv_backend='none')
-def clean(session):
-    paths_to_remove = ['dist', 'build']
-
-    for path in paths_to_remove:
-        if Path(path).exists():
-            shutil.rmtree(path)
-            print(f'Removed {path}/')
-    
-    for item in Path('.').glob('*.egg-info'):
-        if item.is_dir():
-            shutil.rmtree(item)
-            print(f'Removed {item}/')
-    
-    print('Clean complete!')
-
-@nox.session(venv_backend='none')
 def build(session):
     clean(session)
     session.run('python', '-m', 'build')
     print('Build complete!')
+
+@nox.session(venv_backend='none')
+def clean(session):
+    paths_to_remove = ['dist', 'build']
+    for path in paths_to_remove:
+        if Path(path).exists():
+            shutil.rmtree(path)
+            print(f'Removed {path}/')
+    for item in Path('.').glob('*.egg-info'):
+        if item.is_dir():
+            shutil.rmtree(item)
+            print(f'Removed {item}/')
+    print('Clean complete!')
