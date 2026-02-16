054b70ac1b66615c68bb9c262680382bdbccf0c0
Adam Lui <adam@kudoai.com>
2026-02-16 15:21:43 -0800

    Added line break

--- translate-messages/utils/clean.py
+++ translate-messages/utils/clean.py
@@ -4,4 +4,5 @@ import shutil
 for target in ['dist', 'build', '*_cache', '__pycache__', '*.egg-info']:
     for path in Path('.').rglob(target):
         if path.is_dir() : shutil.rmtree(path) ; print(f'Removed {path}/')
+
 print('Clean complete!')
