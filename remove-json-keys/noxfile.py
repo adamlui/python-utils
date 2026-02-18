2a43d5f1aac877a207b224432e5a6051d4bc68c5
Adam Lui <adam@kudoai.com>
2026-02-17 23:56:05 -0800

    Created `push_bump()` helper, used in deploy sessions

--- remove-json-keys/noxfile.py
+++ remove-json-keys/noxfile.py
@@ -1,12 +1,21 @@
 from pathlib import Path
+import sys
 from types import SimpleNamespace as sn
 
 import nox
+
+paths = sn(root=Path(__file__).parent)
+paths.pyproject = paths.root / 'pyproject.toml'
+sys.path.insert(0, str(paths.root / 'utils'))
+
+from lib import toml  # type: ignore
 
 def session(func) : return nox.session(venv_backend='none')(func)
 
 pkg = sn(dir=Path(__file__).parent.name)
 pkg.name = pkg.dir.replace('-', '_')
+
+# SESSIONS
 
 @session
 def test(session) : session.run('py', '-m', pkg.name, *session.posargs, env={ 'PYTHONPATH': 'src' })
@@ -28,11 +37,20 @@ def build(session) : clean(session) ; session.run('py', '-m', 'build') ; print('
 def publish(session) : session.run('bash', 'utils/publish.sh', *session.posargs)
 
 @session
-def deploy_patch(session) : bump_patch(session) ; build(session) ; publish(session)
+def deploy_patch(session) : bump_patch(session) ; push_bump(session) ; build(session) ; publish(session)
 @session
-def deploy_minor(session) : bump_minor(session) ; build(session) ; publish(session)
+def deploy_minor(session) : bump_minor(session) ; push_bump(session) ; build(session) ; publish(session)
 @session
-def deploy_major(session) : bump_major(session) ; build(session) ; publish(session)
+def deploy_major(session) : bump_major(session) ; push_bump(session) ; build(session) ; publish(session)
 
 @session
 def clean(session) : session.run('py', 'utils/clean.py')
+
+# HELPERS
+
+def push_bump(session):
+    new_ver = toml.read('pyproject.toml')['project']['version']
+    session.run('git', 'pull')
+    session.run('git', 'add', '.')
+    session.run('git', 'commit', '-m', f'Bumped {pkg.name} versions to {new_ver}')
+    session.run('git', 'push')
