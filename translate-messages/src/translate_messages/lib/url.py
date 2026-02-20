be5559e38d0ef4be3d1679a22c9d2adcafc14a11
Adam Lui <adam@kudoai.com>
2026-02-20 04:53:07 -0800

    Abstracted `validate_url()`, used in `open()`

--- translate-messages/src/translate_messages/lib/url.py
+++ translate-messages/src/translate_messages/lib/url.py
@@ -3,7 +3,23 @@ from urllib.parse import urlparse
 from urllib.request import urlopen
 import sys, webbrowser
 
-def get(url, timeout=5, encoding='utf-8', allowed_schemes=('http', 'https'), allowed_domains=[]):
+def get(url, timeout=5, encoding='utf-8'):
+    url = validate_url(url)
+    try:
+        with urlopen(url, timeout=timeout) as resp:
+            return resp.read().decode(encoding)
+    except URLError as err:
+        raise RuntimeError(f'Failed to fetch from {url}: {err}')
+
+def open(url):
+    url = validate_url(url)
+    try:
+        webbrowser.open(url)
+    except Exception as err:
+        print(f'Failed to open browser: {err}', file=sys.stderr)
+        sys.exit(1)
+
+def validate_url(url, allowed_schemes=('http', 'https'), allowed_domains=[]):
     parsed_url = urlparse(url)
 
     if parsed_url.scheme not in allowed_schemes:
@@ -13,15 +29,5 @@ def get(url, timeout=5, encoding='utf-8', allowed_schemes=('http', 'https'), all
         input_domain = parsed_url.netloc.lower()
         if not any(input_domain.endswith(allowed_domain.lower()) for allowed_domain in allowed_domains):
             raise ValueError(f"URL domain '{input_domain}' not allowed. Allowed: {allowed_domains}")
-    try:
-        with urlopen(url, timeout=timeout) as resp:
-            return resp.read().decode(encoding)
-    except URLError as err:
-        raise RuntimeError(f'Failed to fetch from {url}: {err}')
-
-def open(url):
-    try:
-        webbrowser.open(url)
-    except Exception as err:
-        print(f'Failed to open browser: {err}', file=sys.stderr)
-        sys.exit(1)
+    
+    return url
