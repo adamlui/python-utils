3f86b69c0a2c218bf3770c1a5a4e9161dee7bb59
Adam Lui <adam@kudoai.com>
2026-02-19 19:29:38 -0800

    Revert "Moved utils/lib/toml.py to src lib/data, created pkg.py"
    This reverts commit 8db884d447e735422ddf5013fc0e42d2a02adda0.

--- translate-messages/src/translate_messages/lib/pkg.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-from . import data
-
-def get_ver() : return data.toml.read(Path(__file__).parent.parent / 'pyproject.toml')
--- translate-messages/utils/bump.py
+++ translate-messages/utils/bump.py
@@ -2,6 +2,8 @@ import argparse, re, sys
 from pathlib import Path
 from types import SimpleNamespace as sn
 
+from lib import toml
+
 paths = sn(root=Path(__file__).parent.parent)
 paths.pyproject = paths.root / 'pyproject.toml'
 paths.readme = paths.root / 'docs/README.md'
@@ -33,7 +35,7 @@ def bump_pyproject_vers(pyproject, project, new_ver):
 
     # Bump project.version
     pyproject['project']['version'] = new_ver
-    data.toml.write(paths.pyproject, pyproject)
+    toml.write(paths.pyproject, pyproject)
     log.success(msgs.log_BUMPED_PROJECT_VER.format(prev_ver=project.version, **locals()))
 
     # Bump project.urls['Releases']
@@ -42,7 +44,7 @@ def bump_pyproject_vers(pyproject, project, new_ver):
     log.data(f'{msgs.log_GENERATED_CLOG_URL}: {changelog_url}')
     log.info(f'{msgs.log_UPDATING_CLOG_URL_IN} pyproject.toml...')
     pyproject['project']['urls']['Changelog'] = changelog_url
-    data.toml.write(paths.pyproject, pyproject)
+    toml.write(paths.pyproject, pyproject)
     log.success(msgs.log_BUMPED_CLOG_URL_VER_TAG.format(**locals()))
 
 def update_readme_vers(new_ver):
@@ -62,7 +64,7 @@ def main():
 
     # Init project data
     log.info(f'{msgs.log_LOADING_PYPROJECT.format(pyproject_path=paths.pyproject)}...')
-    pyproject = data.toml.read(paths.pyproject)
+    pyproject = toml.read(paths.pyproject)
     project = sn(**pyproject['project'])
 
     # Update files
