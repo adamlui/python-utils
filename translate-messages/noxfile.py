2543ab85d88df0e891ea08c1d82906bd481335fd
Adam Lui <adam@kudoai.com>
2026-02-21 16:28:20 -0800

    Extracted `paths`

--- translate-messages/noxfile.py
+++ translate-messages/noxfile.py
@@ -5,6 +5,7 @@ import nox
 
 pkg = sn(dir=Path(__file__).parent.name)
 pkg.name = pkg.dir.replace('-', '_')
+paths = sn(utils=sn(bump='utils/bump.py', clean='utils/clean.py', publish='utils/publish.sh'))
 
 def session(func) : return nox.session(venv_backend='none')(func)
 
@@ -24,12 +25,12 @@ def debug(session) : session.run('py', '-m', pkg.name, '--debug', *session.posar
 
 @session
 def bump_patch(session, no_push=True):
-    cmd = ['py', 'utils/bump.py', '--patch']
+    cmd = ['py', paths.utils.bump, '--patch']
     if no_push : cmd.append('--no-push')
     session.run(*cmd, *session.posargs)
 @session
 def bump_minor(session, no_push=True):
-    cmd = ['py', 'utils/bump.py', '--minor']
+    cmd = ['py', paths.utils.bump, '--minor']
     if no_push : cmd.append('--no-push')
     session.run(*cmd, *session.posargs)
 @session
@@ -37,14 +38,14 @@ def bump_feat(session, no_push=True):
     bump_minor(session, no_push)
 @session
 def bump_major(session, no_push=True):
-    cmd = ['py', 'utils/bump.py', '--major']
+    cmd = ['py', paths.utils.bump, '--major']
     if no_push : cmd.append('--no-push')
     session.run(*cmd, *session.posargs)
 
 @session
 def build(session) : clean(session) ; session.run('py', '-m', 'build') ; print('Build complete!')
 @session
-def publish(session) : session.run('bash', 'utils/publish.sh', *session.posargs)
+def publish(session) : session.run('bash', paths.utils.publish, *session.posargs)
 
 @session
 def deploy_patch(session) : bump_patch(session, no_push=False) ; build(session) ; publish(session)
@@ -56,4 +57,4 @@ def deploy_feat(session) : deploy_minor(session)
 def deploy_major(session) : bump_major(session, no_push=False) ; build(session) ; publish(session)
 
 @session
-def clean(session) : session.run('py', 'utils/clean.py')
+def clean(session) : session.run('py', paths.utils.clean)
