85078efb4292aeedc0b2ad670328b0b39ff63804
Adam Lui <adam@kudoai.com>
2026-02-24 08:37:07 -0800

    Eliminated `translated_msg` init

--- translate-messages/src/translate_messages/lib/language.py
+++ translate-messages/src/translate_messages/lib/language.py
@@ -16,8 +16,7 @@ def create_translations(cli, target_msgs, lang_code):
     for key in src_keys:
 
         if key in cli.config.exclude_keys:
-            translated_msg = cli.en_msgs[key]['message']
-            translated_msgs[key] = { 'message': translated_msg }
+            translated_msgs[key] = { 'message': cli.en_msgs[key]['message'] }
             continue
 
         if key not in target_msgs:
