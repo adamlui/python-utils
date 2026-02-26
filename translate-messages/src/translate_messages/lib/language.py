2a067290be25b0f4df42566a6a36f59c693f389d
Adam Lui <adam@kudoai.com>
2026-02-26 02:30:20 -0800

    Added file content check before read JSON (fixes adamlui/python-utils#19)

--- translate-messages/src/translate_messages/lib/language.py
+++ translate-messages/src/translate_messages/lib/language.py
@@ -58,7 +58,7 @@ def write_translations(cli):
 
         lang_dir_path = cli.locales_path / lang_dir
         msgs_path = lang_dir_path / cli.msgs_filename
-        if msgs_path.exists():
+        if msgs_path.exists() and msgs_path.stat().st_size > 0:
             msgs = data.json.read(msgs_path)
         else:
             msgs = {}
