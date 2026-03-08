e19891871e9535a6f76b2c7f57b18d03de0196f3
Adam Lui <adam@kudoai.com>
2026-03-08 01:54:58 -0800

    Added missing lib/env.py + import + `log.debug()`

--- project-markers/utils/lib/log.py
+++ project-markers/utils/lib/log.py
@@ -1,6 +1,9 @@
 import os, sys
 from types import SimpleNamespace as sn
+from typing import Optional
 if sys.platform == 'win32' : import colorama ; colorama.init() # enable ANSI color support
+
+from . import env
 
 try : terminal_width = os.get_terminal_size()[0]
 except OSError : terminal_width = 80
@@ -28,6 +31,27 @@ def overwrite_print(msg: str, *args, **kwargs) -> None:
 def success(msg: str, *args, **kwargs) -> None : print(f'\n{colors.bg}{msg.format(*args, **kwargs)}{colors.nc}')
 def tip(msg: str, *args, **kwargs) -> None : print(f'\n{colors.bc}TIP: {msg.format(*args, **kwargs)}{colors.nc}')
 def warn(msg: str, *args, **kwargs) -> None : print(f'\n{colors.bo}WARNING: {msg.format(*args, **kwargs)}{colors.nc}')
+
+def debug(msg: str, cli: Optional[sn] = None, *args, **kwargs) -> None:
+    if not env.is_debug_mode() : return
+
+    # Init --debug [target]
+    debug_key=None
+    debug_argidx = sys.argv.index('--debug') if '--debug' in sys.argv else sys.argv.index('-V')
+    if debug_argidx +1 < len(sys.argv) and not sys.argv[debug_argidx +1].startswith('-'):
+        debug_key = sys.argv[debug_argidx +1].replace('-', '_')
+
+    if cli: # init data line
+        if debug_key:
+            data_val = getattr(cli.config, debug_key, f'cli.config key {debug_key!r} {cli.msgs.warn_NOT_FOUND.lower()}')
+        else:
+            data_val = cli.config
+        msg += f'\n{colors.gry}{data_val}{colors.nc}'
+
+    if args: # use 'em
+        msg = msg.format(*args, **kwargs)
+
+    print(f'\n{colors.by}DEBUG: {msg}{colors.nc}')
 
 def trunc(msg: str, end: str = '\n') -> None:
     truncated_lines = [
