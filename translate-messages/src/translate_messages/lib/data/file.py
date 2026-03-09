87f6f0b4848ab1c70c013c8111694864b7927199
Adam Lui <adam@kudoai.com>
2026-03-09 06:29:42 -0700

    Deleted line breaks

--- translate-messages/src/translate_messages/lib/data/file.py
+++ translate-messages/src/translate_messages/lib/data/file.py
@@ -3,11 +3,9 @@ from typing import Union
 
 def atomic_write(file_path: Union[Path, str], data: str, encoding: str ='utf-8') -> None: # to prevent TOCTOU
     import os
-
     file_path = Path(file_path)
     file_path.parent.mkdir(parents=True, exist_ok=True)
     tmp_path = file_path.parent / f'.{file_path.name}.tmp'
-
     try:
         with open(tmp_path, 'w', encoding=encoding) as file:
             file.write(data) ; file.flush() ; os.fsync(file.fileno())
