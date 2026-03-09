bb0948a2edc652cb72e743f80f7c712d70ac8be9
Adam Lui <adam@kudoai.com>
2026-03-08 23:22:14 -0700

    Created non-latin-locales pkg

---
+++ non-latin-locales/src/non_latin_locales/__init__.py
@@ -0,0 +1,6 @@
+import json, os, sys
+
+with open(os.path.join(os.path.dirname(__file__), 'non_latin_locales.json')) as file:
+    non_latin_locales = json.load(file)
+
+sys.modules[__name__] = non_latin_locales
---
+++ non-latin-locales/utils/lib/log.py
@@ -0,0 +1,59 @@
+import os, sys
+from types import SimpleNamespace as sn
+from typing import Optional
+if sys.platform == 'win32' : import colorama ; colorama.init() # enable ANSI color support
+
+from . import env
+
+try : terminal_width = os.get_terminal_size()[0]
+except OSError : terminal_width = 80
+
+colors = sn(
+    nc='\x1b[0m',        # no color
+    br='\x1b[1;91m',     # bright red
+    by='\x1b[1;33m',     # bright yellow
+    bo='\x1b[38;5;214m', # bright orange
+    bg='\x1b[1;92m',     # bright green
+    bc='\x1b[1;96m',     # bright cyan
+    bw='\x1b[1;97m',     # bright white
+    dg='\x1b[32m',       # dark green
+    dy='\x1b[33m',       # dark yellow
+    gry='\x1b[90m'       # gray
+)
+
+def data(msg: str, *args, **kwargs) -> None : print(f'\n{colors.bw}{msg.format(*args, **kwargs)}{colors.nc}')
+def dim(msg: str, *args, **kwargs) -> None : print(f'\n{colors.gry}{msg.format(*args, **kwargs)}{colors.nc}')
+def error(msg: str, *args, **kwargs) -> None : print(f'\n{colors.br}ERROR: {msg.format(*args, **kwargs)}{colors.nc}')
+def info(msg: str, *args, end: str = '', **kwargs) -> None:
+    print(f'\n{colors.by}{msg.format(*args, **kwargs)}{colors.nc}', end=end)
+def overwrite_print(msg: str, *args, **kwargs) -> None:
+    sys.stdout.write('\r' + msg.format(*args, **kwargs).ljust(terminal_width)[:terminal_width])
+def success(msg: str, *args, **kwargs) -> None : print(f'\n{colors.bg}{msg.format(*args, **kwargs)}{colors.nc}')
+def tip(msg: str, *args, **kwargs) -> None : print(f'\n{colors.bc}TIP: {msg.format(*args, **kwargs)}{colors.nc}')
+def warn(msg: str, *args, **kwargs) -> None : print(f'\n{colors.bo}WARNING: {msg.format(*args, **kwargs)}{colors.nc}')
+
+def debug(msg: str, cli: Optional[sn] = None, *args, **kwargs) -> None:
+    if not env.is_debug_mode() : return
+
+    # Init --debug [target]
+    debug_key=None
+    debug_argidx = sys.argv.index('--debug') if '--debug' in sys.argv else sys.argv.index('-V')
+    if debug_argidx +1 < len(sys.argv) and not sys.argv[debug_argidx +1].startswith('-'):
+        debug_key = sys.argv[debug_argidx +1].replace('-', '_')
+
+    if cli: # init data line
+        if debug_key:
+            data_val = getattr(cli.config, debug_key, f'cli.config key {debug_key!r} {cli.msgs.warn_NOT_FOUND.lower()}')
+        else:
+            data_val = cli.config
+        msg += f'\n{colors.gry}{data_val}{colors.nc}'
+
+    if args: # use 'em
+        msg = msg.format(*args, **kwargs)
+
+    print(f'\n{colors.by}DEBUG: {msg}{colors.nc}')
+
+def trunc(msg: str, end: str = '\n') -> None:
+    truncated_lines = [
+        line if len(line) < terminal_width else line[:terminal_width -4] + '...' for line in msg.splitlines()]
+    print('\n'.join(truncated_lines), end=end)
