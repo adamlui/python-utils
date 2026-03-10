be8582ceb47cf60751567ab5656baae253cf1deb
Adam Lui <adam@kudoai.com>
2026-03-10 05:15:34 -0700

    Deleted Py 3 syntax/feats to ensure 2.6 compatibility

--- find-project-root/src/find_project_root/__init__.py
+++ find-project-root/src/find_project_root/__init__.py
@@ -2,4 +2,4 @@ import sys
 
 from .api import find_project_root
 
-sys.modules[__name__] = find_project_root # type: ignore[assignment]
+sys.modules[__name__] = find_project_root # type: ignore
--- find-project-root/src/find_project_root/api.py
+++ find-project-root/src/find_project_root/api.py
@@ -1,19 +1,12 @@
-import os, sys
-from typing import Optional, Union, List
-if sys.version_info >= (3, 4) : from pathlib import Path
-else : Path = str
+import os
 
 import project_markers
 
-def find_project_root(
-    path: Optional[Union[str, Path]] = None,
-    max_depth: int = 9,
-    markers: Optional[List[str]] = None
-) -> Optional[str]:
+def find_project_root(path= None, max_depth= 9, markers= None):
     current_dir = os.getcwd() if path is None else str(path)
     if not os.path.exists(current_dir):
-        raise ValueError(f'Path does not exist: {os.path.abspath(current_dir)}')
-    if not markers : markers = project_markers # type: ignore
+        raise ValueError('Path does not exist: %s' % os.path.abspath(current_dir))
+    if not markers : markers = project_markers
     for _ in range(max_depth):
         try : dir_files = os.listdir(current_dir)
         except (OSError, IOError, PermissionError):
