bc06a3bcb06b24e4d97a51440a62e65f5fb2e620
Adam Lui <adam@kudoai.com>
2026-03-10 06:04:46 -0700

    Added `--py2` option

--- remove-json-keys/utils/clean.py
+++ remove-json-keys/utils/clean.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import shutil
+import shutil, sys
 from types import SimpleNamespace as sn
 
 from remove_json_keys.lib import data, log
@@ -7,11 +7,18 @@ from remove_json_keys.lib import data, log
 def main():
     msgs_path = Path(__file__).parent / 'data/messages.json'
     msgs = sn(**{ key:val['message'] for key,val in data.json.read(msgs_path)['clean'].items() })
-
-    for target in ['dist', 'build', '*_cache', '__pycache__', '*.egg-info', '*.pyc']:
+    targets = ['*.pyc']
+    if '--py2' not in sys.argv:
+        targets.extend(['dist', 'build', '*_cache', '__pycache__', '*.egg-info'])
+    print(targets)
+    for target in targets:
         for path in Path('.').rglob(target):
-            if path.is_dir() : shutil.rmtree(path) ; log.info(f'{msgs.log_REMOVED} {path}/')
-
+            if path.is_dir():
+                shutil.rmtree(path)
+                log.info(f'{msgs.log_REMOVED} {path}/')
+            elif path.is_file():
+                path.unlink()
+                log.info(f'{msgs.log_REMOVED} {path}')
     log.success(f'{msgs.log_CLEAN_COMPLETE}!')
 
 if __name__ == '__main__' : main()
