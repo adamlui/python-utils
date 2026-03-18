d4d7a2da33c297962c5855cf08985014595ddc21
Adam Lui <adam@kudoai.com>
2026-03-18 00:02:29 -0700

    Cached `is_unicode_supported` result, condensed RGB fallback

--- is-unicode-supported/src/is_unicode_supported/api.py
+++ is-unicode-supported/src/is_unicode_supported/api.py
@@ -3,12 +3,18 @@ import os, sys
 import blessed
 
 def is_unicode_supported() -> bool:
+    if hasattr(is_unicode_supported, '_cached'):
+        return is_unicode_supported._cached
+
     if sys.platform == 'win32' and not os.environ.get('WT_SESSION'):
+        is_unicode_supported._cached = False
         return False # legacy Win consoles don't suport wide chars
+
+    # Measure rendered width of wide CJK char
     terminal = blessed.Terminal()
-    with terminal.cbreak(), terminal.hidden_cursor(): # measure rendered width of wide CJK char
+    with terminal.cbreak(), terminal.hidden_cursor():
         r,g,b = terminal.get_bgcolor()
-        if (r,g,b) == (-1,-1,-1) : (r,g,b) = (0,0,0) # get bg failed, init to black
+        if (r,g,b) == (-1,-1,-1) : r = g = b = 0 # get bg failed, init to black
         sys.stdout.write(terminal.color_rgb(r,g,b)) # make text invisible
         _, x1 = terminal.get_location()
         sys.stdout.write('𠀀')
@@ -17,4 +23,6 @@ def is_unicode_supported() -> bool:
         sys.stdout.write('\b' * (x2 - x1) + ' ' * (x2 - x1) + '\b' * (x2 - x1))
         sys.stdout.flush()
         sys.stdout.write(terminal.normal) # restore text colors
-        return x2 - x1 > 1 # terminal rendered wide char as 2 col
+        result = x2 - x1 > 1 # terminal rendered wide char as 2 col
+        is_unicode_supported._cached = result
+        return result
