8064d3f9fa287a6c048b0e62a37bb6f7f4821105
adamlui <adam@kudoai.com>
2026-04-27 17:38:02 -0700

    Made module directly callable again

--- find-project-root/src/find_project_root/__init__.py
+++ find-project-root/src/find_project_root/__init__.py
@@ -1,3 +1,7 @@
-from .api import find_project_root
+import sys
 
-__all__ = ['find_project_root']
+from .api import find_project_root
+from . import cli
+
+sys.modules[__name__].cli = cli # type: ignore
+sys.modules[__name__] = find_project_root # type: ignore
